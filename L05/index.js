2daff176aeda960b221b2f9458b0a2edc61091c5
paugarler <paugarler@alum.us.es>
2021-03-22 22:01:42 +0100

    send paugarler

--- L05/index.js
+++ L05/index.js
@@ -15,6 +15,10 @@ app.get("/hello", (req, res) => {
     res.send("<html><body><h1>GET Hello from this tiny server</h1></body></html>");
 });
 
+app.get("/info/international-tourism", (req, res) => {
+    res.send("<html><body><h1>International-tourism</h1><h3>Fuente de información:<a href='https://data.worldbank.org/indicator/ST.INT.RCPT.XP.ZS?end=2018&most_recent_year_desc=false&start=2018&view=map&year=1995'>https://data.worldbank.org/indicator/ST.INT.RCPT.XP.ZS?end=2018&most_recent_year_desc=false&start=2018&view=map&year=1995</a></h3><table class="default"><tr><th>country</th><th>year</th><th>number-of-arribals</th><th>number-of-departures</th><th>expenditures</th></tr><tr><td>PORTUGAL</td><td>2014</td><td>10,497,000</td><td>1,502,000</td><td>5,213 billion </td></tr><tr><td>FRANCE</td><td>2014</td><td>206,599,000</td><td>31,941,000</td><td>58,464 billion </td></tr><tr><td>PORTUGAL</td><td>2012</td><td>7,503,000</td><td>1,361,000</td><td>4,482 billion </td></tr><tr><td>FRANCE</td><td>2012</td><td>197,522,000</td><td>29,642,000</td><td>50,068 billion </td></tr><tr><td>RUSSIAN FEDERATION</td><td>2010</td><td>22,281,000</td><td>39,232,000</td><td>30,169 billion </td></tr></table></body></html>");
+});
+
 app.post("/hello", (req, res) => {
     res.send("<html><body><h1>POST Hello from this tiny server</h1></body></html>");
 });
