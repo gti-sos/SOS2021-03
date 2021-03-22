dae2627a5dd7c19da4a8d65be82c5adb40741130
Paula0008 <pauulaa.008@gmail.com>
2021-03-22 16:56:30 +0000

    lab 5 punto 2 y 3

---
+++ L05/index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var path = require("path");
+var app = express();
+var port = (process.env.PORT || 10000);
+app.use("/", express.static(path.join(__dirname,"public")));
+app.get("/hello", (req, res) =>{
+	   res.send("<html><body><h1>GET Hello from this tiny server</h1></body></html");
+});
+app.post("/hello", (req, res) =>{
+	   res.send("<html><body><h1>POST Hello from this tiny server</h1></body></html");
+});
+app.listen(port, () => {
+	console.log('Server ready listening on port '+port);
+});
