4b44ee6dcdc15c0707eda4526833c32d139c7c6f
Ayman Alaoui Mhammedi <aymalamha@alum.us.es>
2021-03-22 15:20:23 +0100

    codigo del profesor

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ var app = express();
 
 var port = (process.env.PORT || 10000);
 
-app.get("/cool", (request, response) => {
+app.use("/", express.static(path.join(__dirname,"public")));
+
+app.get("/hello", (req, res) => {
+    res.send("<html><body><h1>GET Hello from this tiny server</h1></body></html>");
+});
+
+app.post("/hello", (req, res) => {
+    res.send("<html><body><h1>POST Hello from this tiny server</h1></body></html>");
+});
+
+/*app.get("/cool", (request, response) => {
 	response.send(cool());
 	console.log("New request to /cool has arrived");
 	
 });
-
+*/
 app.listen(port, () => {
 	console.log("Server ready listening on port " + port);
 })
