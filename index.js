59aceefa5047b6a7052f99bab8b44834609f8024
Ayman Alaoui Mhammedi <aymalamha@alum.us.es>
2021-03-22 23:35:09 +0100

    heroku resolving....

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-/*var cool = require("cool-ascii-faces");
-var express = require("express");
-var app = express();*/
 
 var express = require("express");
 var path = require("path");
@@ -12,19 +9,14 @@ var port = (process.env.PORT || 10000);
 app.use("/", express.static(path.join(__dirname,"public")));
 
 app.get("/hello", (req, res) => {
-    res.send("<html><body><h1>GET Hello from this tiny server</h1></body></html>");
+    res.send("<html><body><h1>GET Hello  this tiny server</h1></body></html>");
 });
 
 app.post("/hello", (req, res) => {
     res.send("<html><body><h1>POST Hello from this tiny server</h1></body></html>");
 });
 
-/*app.get("/cool", (request, response) => {
-	response.send(cool());
-	console.log("New request to /cool has arrived");
-	
-});
-*/
+
 app.listen(port, () => {
 	console.log("Server ready listening on port " + port);
 })
