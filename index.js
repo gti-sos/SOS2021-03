1a9353a7e66f02384734f79b705b434014a46742
Ayman Alaoui Mhammedi <aymalamha@alum.us.es>
2021-03-22 15:11:26 +0100

    fix port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var cool = require("cool-ascii-faces");
 var express = require("express");
 var app = express();
 
-var port = process.env.PORT || 10000;
+var port = (process.env.PORT || 10000);
 
 app.get("/cool", (request, response) => {
 	response.send(cool());
