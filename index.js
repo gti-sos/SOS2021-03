b45d5a34720122c7021d7cb886cb312638560863
paugarler <paugarler@alum.us.es>
2021-04-06 00:16:04 +0200

    pruebas heroku 4

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var app = express();
 
 app.use(express.json());
 
-app.use("/", express.static(path.join(__dirname,"public")));
-
+//app.use("/", express.static(path.join(__dirname,"public")));
+app.use("/", express.static("./public"));
 ////////////////////////////////////////////////////////
 // international tourisms
 ////////////////////////////////////////////////////////
