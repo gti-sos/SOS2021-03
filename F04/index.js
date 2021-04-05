abb12c975d29e65431ab0070e81c348e6c18f9b5
paugarler <paugarler@alum.us.es>
2021-04-05 23:23:00 +0200

    heroku f04

--- F04/index.js
+++ F04/index.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.use(express.json());
 
-app.use("/", express.static("./public"));
+app.use("/", express.static(path.join(__dirname,"public")));
 
 ////////////////////////////////////////////////////////
 // international tourisms
