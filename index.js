cfed84b100252bbd45ecb6022f785eb7a3cd1314
joavilalv <joavilalv@alum.us.es>
2021-04-12 17:13:39 +0200

    Separación api air-pollution y eliminacion de carpeta f04

--- index.js
+++ index.js
@@ -137,117 +137,16 @@ app.listen(PORT, () => {
 	console.log("Server ready at port " + PORT +"!");
 })
 
-////////////////////////////////////////////////////////
-//AIR-POLLUTION
-////////////////////////////////////////////////////////
-var airpollutioninfo = [];
-
-var airpollutioninfopush = [
-    {
-        "country": "AFGANISTAN", 
-        "year": 1990, 
-        "deaths-ambient-particulate-matter-pollution": 46.4465894382846, 
-        "deaths-household-air-pollution-from-solid-fuels": 250.362909742374,
-        "deaths-air-pollution": 299.47730888328
-    }, 
-    {
-        "country": "ANDORRA", 
-        "year": 2015, 
-        "deaths-ambient-particulate-matter-pollution": 16.872355129519, 
-        "deaths-household-air-pollution-from-solid-fuels": 0.262961292500896,
-        "deaths-air-pollution": 19.4461208689732
-    }, 
-    {
-        "country": "BELGIUM", 
-        "year": 2015, 
-        "deaths-ambient-particulate-matter-pollution": 18.3594395851173, 
-        "deaths-household-air-pollution-from-solid-fuels": 0.114447080967493,
-        "deaths-air-pollution": 20.4764687866951
-    }, 
-    {
-        "country": "LITHUANIA", 
-        "year": 2014, 
-        "deaths-ambient-particulate-matter-pollution": 33.2576985134617, 
-        "deaths-household-air-pollution-from-solid-fuels": 1.02744394947467,
-        "deaths-air-pollution": 35.1118618798498
-    }, 
-    {
-        "country": "BENIN", 
-        "year": 1990, 
-        "deaths-ambient-particulate-matter-pollution": 20.0514327372541, 
-        "deaths-household-air-pollution-from-solid-fuels": 150.277007171104,
-        "deaths-air-pollution": 171.975241436558
-    }
-];
-
-app.get(BASE_API_PATH+"/air-pollution", (req, res)=>{
-    res.send(JSON.stringify(airpollutioninfo, null, 2));
-    res.sendStatus(200);
-});
-
-app.get(BASE_API_PATH+"/air-pollution/loadInitialData", (req, res)=>{   
-    for(var i=0;i<airpollutioninfopush.length;i++){
-        airpollutioninfo.push(airpollutioninfopush[i]);
-    }
-    res.send("Loaded Initial Data");
-    res.sendStatus(200);
-});
-
-app.post(BASE_API_PATH+"/air-pollution", (req,res)=>{
-    var newCountry = req.body;
-    console.log("New country to be added: "+ JSON.stringify(newCountry,null,2));
-    airpollutioninfo.push(newCountry);
-    res.sendStatus(201); 
-});
-
-app.get(BASE_API_PATH+"/air-pollution/:country/:year",(req, res)=>{
-    country = req.params.country;
-    var nuevo = [];
-    for(var i=0; i < airpollutioninfo.length; i++){
-        if(airpollutioninfo[i].year==req.params.year&&airpollutioninfo[i].country===country){
-            nuevo.push(airpollutioninfo[i])
-        }
-    }
-    res.send(JSON.stringify(nuevo, null, 2));
-    
-});
-
-app.delete(BASE_API_PATH+"/air-pollution", (req,res)=>{
-    while(airpollutioninfo.length>0){
-        airpollutioninfo.pop();
-    }
-    res.sendStatus(204); 
-});
-
-app.put(BASE_API_PATH+"/air-pollution",(req, res)=>{
-    res.sendStatus(405);
-});
-
-app.post(BASE_API_PATH+"/air-pollution/:country/:year",(req, res)=>{
-    res.sendStatus(405);
-});
-
-app.put(BASE_API_PATH+"/air-pollution/:country/:year",(req, res)=>{
-    for(var i=0; i<airpollutioninfo.length; i++){
-		if(airpollutioninfo[i].country==req.params.country && airpollutioninfo[i].year==req.params.year){
-			airpollutioninfo[i]=req.body;
-		}
-	}
-	res.send("Updated "+ req.params.country+", "+req.params.year);
-	res.sendStatus(200);
-});
-app.delete(BASE_API_PATH+"/air-pollution/:country/:year",(req, res)=>{
-    for(var i=0; i < airpollutioninfo.length; i++){
-        if(airpollutioninfo[i].country == req.params.country && airpollutioninfo[i].year==req.params.year){
-            airpollutioninfo.splice(i, 1);
-            console.log(airpollutioninfo);
-        }
-    }
-    res.send("Deleted "+ req.params.country+", "+req.params.year);
-    res.sendStatus(204)
-
-});
-
+var airpollutionm = require("./air-pollution/air-pollution.js");
+airpollutionm.getAll(app);
+airpollutionm.loadInitialData(app);
+airpollutionm.postAll(app);
+airpollutionm.getOne(app);
+airpollutionm.deleteAll(app);
+airpollutionm.putAll(app);
+airpollutionm.postOne(app);
+airpollutionm.putOne(app);
+airpollutionm.deleteOne(app);
 ////////////////////////////////////////////////////////
 //Quality-of-life
 ////////////////////////////////////////////////////////
