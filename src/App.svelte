e7b3e5c73de3a25f33663623a885ec253f183f95
paugarler <paugarler@alum.us.es>
2021-04-26 22:00:22 +0200

    fo7 joan y mio faltaba una s un 03 ahora si

--- src/App.svelte
+++ src/App.svelte
@@ -47,7 +47,7 @@
 	
 	<ul>
 		<li><a href="https://sos2021-03.herokuapp.com/api/v1/air-pollution">Air-pollutions deaths</a> (developed by Joan Villalonga)</li>
-		<li><a href="https://sos2021-03.herokuapp.com/api/v1/inter-tourisms">Inter Tourism</a> (developed by Paula Garrido Lerma)</li>
+		<li><a href="https://sos2021-03.herokuapp.com/api/v1/international-tourisms">Inter Tourism</a> (developed by Paula Garrido Lerma)</li>
 		<li><a href="https://sos2021-03.herokuapp.com/api/v1/quality-of-life">Quality of life</a> (developed by Ayman)</li>
 	</ul>
 
