c1e38878cf6b44f9760aa512d1d0dae1ba403b5b
joavilalv <joavilalv@alum.us.es>
2021-04-26 19:15:55 +0200

    hay que volver a hacer algunos cambios basicos

---
+++ svelte-template/src/air-pollution/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	
+    let contacts = []
+    async function getElements(){
+        console.log("Fetching registers ...");
+        const res = await fetch("/elements");
+        if(res.ok){
+            console.log("OK!");
+            const json = await res.json();
+            elements = json;
+            console.log('All done!');
+        }else{
+            console.log("Error!");
+        }
+    }
+    getElements();
+
+</script>
+
+<main>
+	<h1>Muertes por contaminacion del aire</h1>
+	
+</main>
+
