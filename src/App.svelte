b939e6ce49c64fdb39febccab22f6f5caef61222
joavilalv <joavilalv@alum.us.es>
2021-04-26 21:18:08 +0200

    joavilalv listo?

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,31 @@
-<script>
-	export let name;
-</script>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Readme fichero</title>
+</head>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>## SOS2021-03</h1>
+
+ <a href="https://github.com/joavilalv"> - [Joan Villalonga Alvaro](https://github.com/joavilalv)</a>
+ <div> - [Paula Garrido Lerma](https://github.com/Paula0008)</div>
+ <div> - [Ayman Alaoui Mhammedi](https://github.com/AymanAlaouiMhammedi)</div>
+ <div> - *Project description*: Nuestras fuentes de información están orientadas a analizar la relación que existe entre como la calidad de vida puede influenciar al turismo y este ver si al viajar más, es decir, se usan más transportes, hace que aumente la contaminación del aire y las muertes que esto conlleva.</div>
+ <div> - *Repository*: [gti-sos/SOS2021-03](https://github.com/gti-sos/SOS2021-03.git)</div>
+ <div> - *URL*: [http://sos2021-03.herokuapp.com](http://sos2021-03.herokuapp.com)</div>
+ <div> -  *APIs*:</div>
+  <div><a href="https://documenter.getpostman.com/view/14974548/TzJoE1G7">- POSTMAN: https://documenter.getpostman.com/view/14974548/TzJoE1G7</a>
+  <a href= "https://sos2021-03.herokuapp.com/api/v1/air-pollution">[https://sos2021-03.herokuapp.com/api/v1/air-pollution](https://sos2021-03.herokuapp.com/api/v1/air-pollution) (developed by [Joan Villalonga Alvaro](https://github.com/joavilalv))</a></div>
+  <div><a href="https://documenter.getpostman.com/view/9683446/TzJoE1Zk">- POSTMAN: https://documenter.getpostman.com/view/9683446/TzJoE1Zk</a>
+  <a href= "https://sos2021-03.herokuapp.com/api/v1/international-tourisms">[https://sos2021-03.herokuapp.com/api/v1/international-tourisms](https://sos2021-03.herokuapp.com/api/v1/international-tourisms) (developed by [Paula Garrido Lerma](https://github.com/Paula0008))</a></div>
+  <div><a href="https://documenter.getpostman.com/view/14967959/TzJoF1Kp">- POSTMAN: https://documenter.getpostman.com/view/9683446/TzJoE1Zk</a>
+  <a href= "https://sos2021-03.herokuapp.com/api/v1/quality-of-life">[https://sos2021-03.herokuapp.com/api/v1/quality-of-life](https://sos2021-03.herokuapp.com/api/v1/international-tourisms) (developed by [Ayman Alaoui Mhammedi](https://github.com/AymanAlaouiMhammedi))</a></div>
+ 
+	<ul>
+		<li><a href="https://sos2021-10.herokuapp.com/api/v1/air-pollution">Air-pollutions deaths</a> (developed by Joan Villalonga)</li>
+		<li><a href="https://sos2021-10.herokuapp.com/api/v1/inter-tourism">Inter Tourism</a> (developed by Paula Garrido Lerma)</li>
+		<li><a href="https://sos2021-10.herokuapp.com/api/v1/quality-of-life">Quality of life</a> (developed by Ayman)</li>
+	</ul>
 </main>
 
 <style>
@@ -28,3 +49,4 @@
 		}
 	}
 </style>
+</html>
--- src/air-pollution/App.svelte
+++ src/air-pollution/App.svelte
@@ -1,24 +1,67 @@
 <script>
+	import {
+		onMount
+	} from "svelte";
 	
-    let contacts = []
-    async function getElements(){
-        console.log("Fetching registers ...");
-        const res = await fetch("/elements");
+	import Table from "sveltestrap/src/Table.svelte";
+	let air_pollution = [];
+	
+	
+	async function RegisterData() {
+    	console.log("Loading data...");
+   		const res = await fetch("/api/v1/air-pollution/loadInitialData");
+		
         if(res.ok){
-            console.log("OK!");
-            const json = await res.json();
-            elements = json;
-            console.log('All done!');
-        }else{
-            console.log("Error!");
-        }
-    }
-    getElements();
-
+			console.log("Ok.");
+			getRegisters();
+		}else{
+			console.log("Error");
+		}
+  	}
+	
+	async function getRegisters() {
+    	console.log("Fetching data...");
+   		const res = await fetch("/api/v1/air-pollution/");
+		
+        if(res.ok){
+			console.log("Ok.");
+			const json = await res.json();
+			air_pollution = json;
+			console.log(`We have ${air_pollution.length} air_pollution.`)
+		}else{
+			console.log("Error");
+		}
+  	}
+	
+	onMount(getRegisters);
+	
 </script>
 
 <main>
-	<h1>Muertes por contaminacion del aire</h1>
-	
+	<Table bordered>
+		<thead>
+			<tr>
+				<td>Pais</td>
+				<td>Año</td>
+				<td>Muertes por contaminación del aire de particulas</td>
+				<td>Muertes por contaminación del aire por combustibles sólidos</td>
+				<td>Muertes por contaminación del aire</td>
+			</tr>
+		</thead>
+		<tbody>
+			{#each air_pollution as r}
+				<tr>
+				<td>{r.country}</td>
+				<td>{r.year}</td>
+				<td>{r.deaths_ambient_particulate_matter_pollution}</td>
+				<td>{r.deaths_household_air_pollution_from_solid_fuels}</td>
+				<td>{r.deaths_air_pollution}</td>
+				
+				
+				</tr>
+			{/each}
+			
+		</tbody>
+	</Table>
 </main>
 
