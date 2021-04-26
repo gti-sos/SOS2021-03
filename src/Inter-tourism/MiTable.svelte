5c088033a9f40685216cfdce3e248e25b9bf1470
paugarler <paugarler@alum.us.es>
2021-04-26 23:33:53 +0200

    fo7 joan y mio me muerro

--- src/Inter-tourism/MiTable.svelte
+++ src/Inter-tourism/MiTable.svelte
@@ -1,58 +1,66 @@
 <script>
-    import {
-        onMount
+	import {
+		onMount
 	} from "svelte";
+	
+	import Table from "sveltestrap/src/Table.svelte";
+	let inter_tourism = [];
+	
+	
+	async function RegisterData() {
+    	console.log("Loading data...");
+   		const res = await fetch("/api/v1/international-tourisms/loadInitialData");
 		
-    import Table from "sveltestrap/src/Table.svelte";
-    let intertourisms=[];
-    async function getIntertourisms() {
-        console.log("Loading tourisms...");
-        const res = await fetch("/api/v1/international-tourisms/loadInitialData");
         if(res.ok){
-            console.log("Ok.");
-            getRegisters(); 
-        }else{
-            console.log("Error");
-        }
-    }
-    async function getRegisters() {
+			console.log("Ok.");
+			getRegisters();
+		}else{
+			console.log("Error");
+		}
+  	}
+	
+	async function getRegisters() {
     	console.log("Fetching data...");
    		const res = await fetch("/api/v1/international-tourisms/");
 		
         if(res.ok){
 			console.log("Ok.");
 			const json = await res.json();
-			intertourisms = json;
-			console.log(`We have ${intertourisms.length} intertourisms.`)
+			inter_tourism = json;
+			console.log(`We have ${inter_tourism.length} inter tourism.`)
 		}else{
 			console.log("Error");
 		}
   	}
-    onMount(getRegisters);
+	
+	onMount(getRegisters);
+	
 </script>
 
 <main>
-    <Table bordered>
-        <thread> 
-            <tr>
-                <td>País</td>
-                <td>Año</td>
-                <td>Número de llegadas</td>
-                <td>Número de salidas</td>
-                <td>Gasto en billones</td>
-            </tr>
-        </thread>
-        <tbody>
-            {#each intertourisms as datatourism}
-                <tr>
-                    <td>{datatourism.country}</td>
-                    <td>{datatourism.year}</td>
-                    <td>{datatourism.numberofarribals}</td>
-                    <td>{datatourism.numberofdepartures}</td>
-                    <td>{datatourism.expendituresbillion}</td>
-                </tr>
-            {/each}
-        </tbody>
-    </Table>
-
+	<Table bordered>
+		<thead>
+			<tr>
+				<td>Pais</td>
+				<td>Año</td>
+				<td>Número de llegadas</td>
+				<td>Número de salidas</td>
+				<td>Gastos en billones</td>
+			</tr>
+		</thead>
+		<tbody>
+			{#each inter_tourism as r}
+				<tr>
+				<td>{r.country}</td>
+				<td>{r.year}</td>
+				<td>{r.numberofarribals}</td>
+				<td>{r.numberofdepartures}</td>
+				<td>{r.expendituresbillion}</td>
+				
+				
+				</tr>
+			{/each}
+			
+		</tbody>
+	</Table>
 </main>
