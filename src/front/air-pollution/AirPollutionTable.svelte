16fa49fdfebbbdeba2d59663dc50a274537aefe9
joavilalv <joavilalv@alum.us.es>
2021-05-07 12:10:05 +0200

    joavilalv intento 5 de d02//busquedas prueba arreglo

--- src/front/air-pollution/AirPollutionTable.svelte
+++ src/front/air-pollution/AirPollutionTable.svelte
@@ -159,7 +159,7 @@
 			console.log("OK");
 			const json = await res.json();
             console.log(json);
-			air_pollution = json;	
+			air_pollution=[json];	
             console.log("air_pollution now is that:");
             console.log(air_pollution);	
             console.log(air_pollution.length);
